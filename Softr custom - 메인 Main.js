2a6708a455fb83e635416f6a8f17eebe771fd20d
crepelove <42159665+crepelove@users.noreply.github.com>
2025-08-12 16:03:51 +0900

    Add files via upload

---
+++ Softr custom - 메인 Main.js
@@ -0,0 +1,43 @@
+
+// 메인페이지 Main
+
+// 네이버검색 키워드 삽입
+$('head')[0].insertAdjacentHTML('afterbegin', '<meta name="naver-site-verification" content="84658af69ac089b37ea232a7ec43923bb59a3b87" />');
+
+// footer 디자인
+window.addEventListener('block-loaded-1-main-footer1', function () {
+  infoP = document.querySelectorAll("#\\31 -main-footer1 > div:nth-child(1) > footer > div > div:nth-child(1) > p")[0];
+  infoP.innerHTML = `<span style="font-weight: 700;font-size: 1rem;">(주)버스크</span>
+<span style="font-weight: 500;font-size: 1rem;">Committed partner to your global business</span>
+대표이사 : 조호성<br>본사 : 경기도 수원시 영통구 청명로 87, 운현빌딩 202호<br>서울사무소 : 서울특별시 강남구 테헤란로2길 36, 3층 304호<br>물류집하지 : 경기도 김포시 통진읍 서암고정로 162`;
+  // 대표이사 : 조호성<br>본사 : 경기도 수원시 영통구 청명로 87, 운현빌딩 202호<br>서울사무소 : 서울특별시 강남구 테헤란로2길 36, 3층 301호<br>Fax : 031-239-4166<br>E-mail : contact@buskinc.com`;
+  // 대표이사 : 조호성<br>본사 : 경기도 수원시 영통구 청명로 87, 운현빌딩 202호<br>서울사무소 : 서울특별시 강남구 테헤란로2길 36, 3층 301호<br>Tel : 070-7954-6070 | Fax : 031-239-4166<br>E-mail : contact@buskinc.com`;
+
+  // infoP.innerHTML = `(주)버스크  Committed partner to your global business <br>대표이사 : 조호성 <br>본사 : 경기도 수원시 영통구 청명로 87, 운현빌딩 202호<br>서울사무소 : 서울특별시 강남구 테헤란로2길 36, 3층 301호<br>Tel : 070-7954-6070 | Fax : 031-239-4166<br>E-mail : contact@buskinc.com`;
+  //   infoP.innerHTML = `(주)버스크  Committed partner to your global business 
+  // 대표이사 : 조호성
+  // 본사 : 경기도 수원시 영통구 청명로 87, 운현빌딩 202호
+  // 서울사무소 : 서울특별시 강남구 테헤란로2길 36, 3층 301호
+  // Tel : 070-7954-6070 | Fax : 031-239-4166
+  // E-mail : contact@buskinc.com`;
+
+
+  // p1 = document.querySelector("#\\31 -main-footer1 > div:nth-child(1) > footer > div > div._743d8e5_7qws3sm > ul > li:nth-child(1) > a > div");
+  // p2 = document.querySelector("#\\31 -main-footer1 > div:nth-child(1) > footer > div > div._743d8e5_7qws3sm > ul > li:nth-child(2) > a > div");
+  // p3 = document.querySelector("#\\31 -main-footer1 > div:nth-child(1) > footer > div > div._743d8e5_7qws3sm > ul > li:nth-child(3) > a > div");
+  // p1.after('네이버');
+  // p2.after('페이스북');
+  // p3.after('인스타그램');
+});
+
+
+<style>
+  {/* #\31-main-footer1 > div:nth-child(1) > footer > div {
+    column-gap : 0px;
+  } 
+  #\31-main-footer1 > div:nth-child(1) > footer > div > div:first-child {
+      width: 400px;
+  }*/}
+</style>
+
+{/* ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ */ }
